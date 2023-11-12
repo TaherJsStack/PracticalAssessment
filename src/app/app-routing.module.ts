80c81ad2bc59cb94d39f5c0230619acac7a0d6df
TaherJsStack <t.taher.php@gmail.com>
2023-11-12 03:06:45 +0200

    update app routing module with new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    title: 'products',
+    loadChildren: () =>
+      import("./pages/products/products.module").then(
+        (m) => m.ProductsModule
+      ),
+  },
+  {
+    path: "orders",
+    title: 'orders',
+    loadChildren: () =>
+      import("./pages/orders/orders.module").then(
+        (m) => m.OrdersModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
