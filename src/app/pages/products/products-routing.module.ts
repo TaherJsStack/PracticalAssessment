f4ebc3d983b6cf76d3cb054d3de43a147be6e434
TaherJsStack <t.taher.php@gmail.com>
2023-11-12 03:02:58 +0200

    create products pages with children - create module for products - create routing module and update routes for child component - create nested component page for product [ list ]

---
+++ src/app/pages/products/products-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductsComponent } from './products.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductsComponent,
+    children: [
+      {
+        path: '',
+        component: ProductsListComponent,
+      },
+      {
+        path: 'products',
+        component: ProductsListComponent,
+      },
+  ]}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProductsRoutingModule { }
